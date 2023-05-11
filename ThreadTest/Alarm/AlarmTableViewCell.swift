b882837fd8f1b43d30ff69560dce3b4e9f2401b5
leeseoyoung0822 <822angel@naver.com>
2023-05-11 15:32:50 +0900

    clockapp

---
+++ ThreadTest/Alarm/AlarmTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlarmTableViewCell.swift
+//  ThreadTest
+//
+//  Created by 이서영 on 2023/05/11.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ThreadTest/TempViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TempViewController.swift
+//  ThreadTest
+//
+//  Created by 이서영 on 2023/05/10.
+//
+
+import UIKit
+
+class TempViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
